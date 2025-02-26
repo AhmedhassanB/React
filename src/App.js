fa0cfe210d7a377dc42f2d4a013a5c4e788a268e
Ahmed Hassan <amh247408@gmail.com>
2025-02-26 13:52:30 +0200

    Add movie search functionality and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import { useEffect } from "react";
+import "./App.css";
+import searchIcons from "./searchMovies";
 // fe923914
+const API_URL = "https://www.omdbapi.com/?apikey=fe923914";
 const App = () => {
-  const API_URL = "https://www.omdbapi.com/?apikey=fe923914";
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s={title}`); // will call the api
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    //call the function search movie
+    searchMovies("avengers");
+  }, []);
   return (
     <div className="App">
-      <h1>welcome to the world of React</h1>
+      <h1>Movie Land</h1>
     </div>
   );
 };
