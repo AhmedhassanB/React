8e3ddf2c40f8721a1d894f87b4dfd5ceecd80565
Ahmed Hassan <amh247408@gmail.com>
2025-02-26 12:04:14 +0200

    state and handle Event

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
 import "./App.css";
-
-const Example = () => {
-  const name = "ahmed";
-  const age = 12;
-  const gender = "Male";
-  return (
-    <div>
-      <h1>my name is {name}</h1>
-      <h1>my age is {age}</h1>
-      <h1>my gender is {gender}</h1>
-    </div>
-  );
-};
+import { useState } from "react";
+// const Example = (props) => {
+//   const name = props.name;
+//   const age = props.age;
+//   const gender = props.gender;
+//   return (
+//     <div>
+//       <h1>my name is {name}</h1>
+//       <h1>my age is {age}</h1>
+//       <h1>my gender is {gender}</h1>
+//     </div>
+//   );
+// };
 
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <Example />
+      {/* <Example name="ahmed" age="20" gender="Male" /> */}
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        +
+      </button>
+      <h1>{count}</h1>
+      <button
+        onClick={() => {
+          setCount(count - 1);
+        }}
+      >
+        -
+      </button>
     </div>
   );
 };
